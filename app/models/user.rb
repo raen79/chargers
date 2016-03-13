6516f41ec5ab0ac8cba18d04b627c767c29fb612
Ioana Surdu-Bob <ioana.sbob@gmail.com>
2016-03-13 01:17:21 +0000

    fixed delete issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :chargers
-  has_many :raitings
+  has_many :raitings, dependent: :destroy 
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
